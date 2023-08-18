534256044e328625d6f9439a577d65e1ec31981b
redframelbx <139869040+redframelbx@users.noreply.github.com>
2023-08-19 00:30:01 +0800

    sales by hour into histogram

--- dashboard.py
+++ dashboard.py
@@ -112,7 +112,7 @@ fig_sales_date.update_layout(
 
 #create sales by hour graph
 sales_by_hour = df_selection.groupby(by=['hour'])[['Total']].sum()
-fig_hourly_sales = px.bar(
+fig_hourly_sales = px.histogram(
     sales_by_hour,
     x = sales_by_hour.index,
     y='Total',
