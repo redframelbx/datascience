e26c6b191e85109e173611c050788bdc54189d2e
redframelbx <139869040+redframelbx@users.noreply.github.com>
2023-08-19 00:03:56 +0800

    line112

--- dashboard.py
+++ dashboard.py
@@ -109,7 +109,7 @@ fig_sales_date.update_layout(
     xaxis=(dict(showgrid=True))
 
 #create sales by hour graph
-sales_by_hour = df_selection.groupby(by=['hour']).sum()[['Total']]
+sales_by_hour = df_selection.groupby(by=['hour'])[['Total']].sum()
 fig_hourly_sales = px.bar(
     sales_by_hour,
     x = sales_by_hour.index,
