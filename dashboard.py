06e3cbe697f6fbada1794fee4be2f0e80e88e48f
redframelbx <139869040+redframelbx@users.noreply.github.com>
2023-08-19 22:24:46 +0800

    92

--- dashboard.py
+++ dashboard.py
@@ -89,7 +89,7 @@ fig_product_sales = px.bar(
     y=sales_by_product_line.index,
     orientation='h',
     title = '<b>Sales by Product LIne</b>',
-    color_discrete_sequence=['#008388'] * len(sales_by_product_line),
+    # color_discrete_sequence=['#008388'] * len(sales_by_product_line),
     template='plotly',
 )
 fig_product_sales.update_layout(
